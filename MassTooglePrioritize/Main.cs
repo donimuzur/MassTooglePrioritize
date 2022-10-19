c990135f119f82586d7424eb038ed49e3675fa17
Muhammad Zulfi Rusdani <donimuzur@gmail.com>
2022-10-19 16:15:19 +0700

    init

---
+++ MassTooglePrioritize/Main.cs
@@ -0,0 +1,54 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace MassTooglePrioritize
+{
+	public static class BuildInfo
+	{
+		public const string Name = "MassTooglePrioritize";
+		public const string Description = "mass toogle prioritize with the same type Build site ";
+		public const string Author = "donimuzur"; // Author of the Mod.  (MUST BE SET)
+		public const string Company = null; // Company that made the Mod.  (Set as null if none)
+		public const string Version = "1.0.3"; // Version of the Mod.  (MUST BE SET)
+		public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)    
+	}
+	public class MassTooglePrioritize:MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+			MelonLogger.Msg("MassTooglePrioritizeMod started");
+        }
+        public override void OnUpdate()
+        {
+			if (Input.GetMouseButtonDown(1))
+			{
+				var gameManager = GameObject.FindObjectOfType<GameManager>();
+				if (gameManager == null)
+				{
+					return;
+				}
+				var selectedObj = GameObject.FindObjectOfType<UIBuildsiteWindow_New>();
+				if (selectedObj != null)
+				{
+					bool prioritizedToggle = !selectedObj.prioritizedToggle.isOn;
+					var objectToPrioritizeList = GameObject.FindObjectsOfType<BuildSite>();
+					foreach (var objectToPrioritize in objectToPrioritizeList)
+					{
+						var buildSiteResource = objectToPrioritize.GetComponent<BuildSiteResource>();
+						var selectedBuildSiteResource = selectedObj.buildsiteResource;
+						if(buildSiteResource != null && selectedBuildSiteResource != null)
+                        {
+							var selectedConstructionData = selectedBuildSiteResource.constructionData;
+							if (buildSiteResource.prefabToConstruct.name == selectedConstructionData.prefabToConstruct.name)
+							{
+								objectToPrioritize.prioritizeConstruction = prioritizedToggle;
+								selectedObj.prioritizedToggle.isOn = prioritizedToggle;
+							}
+						}
+						
+					}
+				}
+			}
+		}
+    }
+}
---
+++ MassTooglePrioritize/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(MassTooglePrioritize.BuildInfo.Description)]
+[assembly: AssemblyDescription(MassTooglePrioritize.BuildInfo.Description)]
+[assembly: AssemblyCompany(MassTooglePrioritize.BuildInfo.Company)]
+[assembly: AssemblyProduct(MassTooglePrioritize.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + MassTooglePrioritize.BuildInfo.Author)]
+[assembly: AssemblyTrademark(MassTooglePrioritize.BuildInfo.Company)]
+[assembly: AssemblyVersion(MassTooglePrioritize.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(MassTooglePrioritize.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(MassTooglePrioritize.MassTooglePrioritize), MassTooglePrioritize.BuildInfo.Name, MassTooglePrioritize.BuildInfo.Version, MassTooglePrioritize.BuildInfo.Author, MassTooglePrioritize.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
